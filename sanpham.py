934577e7ed9ab51dd4c57bb14f756a5a8ad61c81
thphuc1011 <thphuc1011@gmail.com>
2025-10-11 08:12:14 +0700

    lab5

--- sanpham.py
+++ sanpham.py
@@ -1,17 +1,29 @@
 class SanPham:
     def __init__(self, ten_san_pham, gia, giam_gia):
-        self.ten_san_pham = ten_san_pham
-        self.gia = gia
+        self.__ten_san_pham = ten_san_pham
+        self.__gia = gia
         self.__giam_gia = giam_gia
 
+    def get_ten_sp(self):
+        return self.__ten_san_pham
+    def set_ten_sp(self, ten_sp):
+        self.__ten_san_pham = ten_sp
+    def get_gia_sp(self):
+        return self.__gia
+    def set_gia_sp(self, gia_sp):
+        self.__gia = gia_sp
+    def get_giam_gia_sp(self):
+        return self.__giam_gia
+    def set_giam_gia_sp(self, giam_gia_sp):
+        self.__giam_gia = giam_gia_sp
 
     def thue_nhap_khau(self):
         return self.gia * 0.1
     def nhap_thong_tin_sp(self):
-        self.ten_san_pham = input("Tên sản phẩm: ")
-        self.gia = float(input("Giá sản phẩm: "))
+        self.__ten_san_pham = input("Tên sản phẩm: ")
+        self.__gia = float(input("Giá sản phẩm: "))
         self.__giam_gia = float(input("Giảm giá sản phẩm: "))
-    def xuat_thong_tin_sp(self):
-        print(f"Sản phẩm {self.ten_san_pham} có giá {self.gia} được giảm giá {self.__giam_gia} và thuế nhập khẩu {self.thue_nhap_khau()}")
     def __str__(self):
-        return f"Sản phẩm {self.ten_san_pham} có giá {self.gia} được giảm giá {self.__giam_gia} và thuế nhập khẩu {self.thue_nhap_khau()}"
+        thongtin = f"""Sản phẩm {self.__ten_san_pham} có giá {self.__gia} được giảm giá {self.__giam_gia} và thuế nhập khẩu {self.thue_nhap_khau()}"""
+        return thongtin
+    
